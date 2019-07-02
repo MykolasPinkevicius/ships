aa42751a08c3a1ec9cab58cadaff85e9adab75d0
Mykolas Pinkevičius <mdpbeats@gmail.com>
2019-07-02 14:48:30 +0300

    Updated configurations

--- src/main/java/model/Arrival.java
+++ src/main/java/model/Arrival.java
@@ -1,9 +1,24 @@
 package model;
 
-        import java.util.Date;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlTransient;
+import java.util.Date;
 
+@Entity
+@Table(name = "arrivals")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Arrival {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @XmlTransient
+    private Long id;
+    @NotNull
     private String Port;
+    @NotNull
     private Date date;
 
     public String getPort() {
--- src/main/java/model/Logbook.java
+++ src/main/java/model/Logbook.java
@@ -2,7 +2,6 @@ package model;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement
 public class Logbook {
 
     private Departure departure;
@@ -10,12 +9,45 @@ public class Logbook {
     private Arrival arrival;
     private EndOfFishing endOfFishing;
 
-    public Logbook() {};
-
     public Logbook(Departure departure, Catch aCatch, Arrival arrival, EndOfFishing endOfFishing) {
         this.departure = departure;
         this.aCatch = aCatch;
         this.arrival = arrival;
         this.endOfFishing = endOfFishing;
     }
+
+    public Departure getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(Departure departure) {
+        this.departure = departure;
+    }
+
+    public Catch getaCatch() {
+        return aCatch;
+    }
+
+    public void setaCatch(Catch aCatch) {
+        this.aCatch = aCatch;
+    }
+
+    public Arrival getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(Arrival arrival) {
+        this.arrival = arrival;
+    }
+
+    public EndOfFishing getEndOfFishing() {
+        return endOfFishing;
+    }
+
+    public void setEndOfFishing(EndOfFishing endOfFishing) {
+        this.endOfFishing = endOfFishing;
+    }
+
+    public Logbook() {};
+
 }
