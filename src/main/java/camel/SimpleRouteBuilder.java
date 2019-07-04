a72bc880cd0484e02bca750225df3f82a09345e8
Mykolas Pinkevičius <mdpbeats@gmail.com>
2019-07-04 12:58:31 +0300

    Camel message transfer from end point to end point

--- src/main/java/camel/SimpleRouteBuilder.java
+++ src/main/java/camel/SimpleRouteBuilder.java
@@ -1,13 +1,13 @@
 package camel;
 
-import model.Logbook;
+import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.model.dataformat.JacksonXMLDataFormat;
-import org.apache.camel.model.dataformat.JsonDataFormat;
+import org.apache.camel.impl.DefaultCamelContext;
 
 public class SimpleRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-       JsonDataFormat jsonDataFormat = new JsonDataFormat();
+        from("file://C:\\Program Files\\wildfly-9.0.2.Final\\wildfly-9.0.2.Final\\bin\\?fileName=logbook.log")
+                .to("file://C:\\Program Files\\wildfly-9.0.2.Final\\wildfly-9.0.2.Final\\?fileName=logbook.log");
     }
 }
